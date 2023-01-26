7cecf5777cea182a288bc3a75d5b33fd6cea277c
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-26 23:51:36 +0500

    Update

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Task from './Components/Task';
 
 export default function App() {
   return (
-    <>
-      <View style={{backgroundColor: '#e4e6e9'}}>
-        <Text
-          style={{
-            color: '#000',
-            fontWeight: "bold",
-            fontSize: 24,
-            marginTop: 110,
-            padding: 20,
-          }}>
-          Today's tasks
-        </Text>
-        <View>
-
-        </View>
+    <View style={{backgroundColor: '#e4e6e9', padding: 20}}>
+      <Text
+        style={{
+          color: '#000',
+          fontWeight: 'bold',
+          fontSize: 24,
+          marginTop: 70,
+          marginBottom: 30
+        }}>
+        Today's tasks
+      </Text>
+      <View>
+        <Task text="1" />
+        <Task text="2" />
+        <Task text="3" />
+        <Task text="4" />
+        <Task text="5" />
       </View>
-    </>
+    </View>
   );
 }
--- Components/Task.js
+++ Components/Task.js
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
 
-export default function Task() {
+export default function Task(props) {
   return (
-    <View>
-      <Text>Task</Text>
+    <View
+      style={{
+        padding: 15,
+        borderRadius: 10,
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+        marginBottom: 20,
+      }}>
+      <View
+        style={{flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap'}}>
+        <View
+          style={{
+            backgroundColor: '#55bcf6',
+            height: 24,
+            width: 24,
+            opacity: 0.4,
+          }}></View>
+      </View>
+      <Text>{props.text}</Text>
+      <View
+        style={{
+          height: 12,
+          width: 12,
+          borderColor: '#55bcf6',
+          borderWidth: 2,
+          borderRadius: 5,
+        }}></View>
     </View>
-  )
+  );
 }
