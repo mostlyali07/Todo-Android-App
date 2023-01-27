e7c459acd2dedafd5b9e5a6a0e68d90b135ba6ba
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-27 17:00:21 +0500

    Update

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import React from 'react';
 import Task from './Components/Task';
 
@@ -11,7 +18,7 @@ export default function App() {
           fontWeight: 'bold',
           fontSize: 24,
           marginTop: 70,
-          marginBottom: 30
+          marginBottom: 30,
         }}>
         Today's tasks
       </Text>
@@ -22,6 +29,45 @@ export default function App() {
         <Task text="4" />
         <Task text="5" />
       </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{
+          position: 'absolute',
+          bottom: 60,
+          width: '100%',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+        }}>
+        <TextInput
+          style={{
+            paddingVertical: 15,
+            paddingHorizontal: 15,
+            backgroundColor: '#fff',
+            borderRadius: 60,
+            borderColor: '#c0c0c0',
+            borderWidth: 1,
+            width: 250,
+          }}
+          placeholder={'Write a task'}
+        />
+        <TouchableOpacity>
+          <View
+            style={{
+              width: 60,
+              height: 60,
+              backgroundColor: '#fff',
+              borderRadius: 60,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderColor: '#c0c0c0',
+              borderWidth: 1,
+            }}>
+            <Text style={{}}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
